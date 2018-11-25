24e051fc55ff4be1e040ddaca9d997cb6bbb463f
batkoivo <unikfirst@gmail.com>
2018-11-25 12:27:26 +0100

    feat:(routing) feat:(nav styling)

--- c/src/app/app.module.ts
+++ c/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { GraphComponent } from './graph/graph.component';
-import { StatsComponent } from './stats/stats.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    GraphComponent,
-    StatsComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
